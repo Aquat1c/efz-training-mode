4f0c1e8f2acfd7a0f6e3216d894ea09f83f34eb6
Aquat1c <mitshaforever@gmail.com>
2025-08-12 22:40:28 +0300

    Fixed regular motions

--- include/input/motion_constants.h
+++ include/input/motion_constants.h
@@ -24,8 +24,18 @@
 #define MOTION_421A      209  // Down-Back-Down + A
 #define MOTION_421B      210  // Down-Back-Down + B
 #define MOTION_421C      211  // Down-Back-Down + C
-
-
+#define MOTION_236236A   212  // Double QCF + A (super)
+#define MOTION_236236B   213  // Double QCF + B (super)
+#define MOTION_236236C   214  // Double QCF + C (super)
+#define MOTION_214214A   215  // Double QCB + A (super)
+#define MOTION_214214B   216  // Double QCB + B (super)
+#define MOTION_214214C   217  // Double QCB + C (super)
+#define MOTION_CHARGE_BACK_FORWARD_A   220  // Charge [4]6+A
+#define MOTION_CHARGE_BACK_FORWARD_B   221  // Charge [4]6+B
+#define MOTION_CHARGE_BACK_FORWARD_C   222  // Charge [4]6+C
+#define MOTION_CHARGE_DOWN_UP_A        223  // Charge [2]8+A
+#define MOTION_CHARGE_DOWN_UP_B        224  // Charge [2]8+B
+#define MOTION_CHARGE_DOWN_UP_C        225  // Charge [2]8+C
 
 // Super move motion constants
 #define MOTION_41236A    300  // HCF + A
