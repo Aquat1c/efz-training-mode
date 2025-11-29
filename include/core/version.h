766f194d87d222e3be1a2923154b11ac27247605
Aquat1c <mitshaforever@gmail.com>
2025-11-29 05:05:29 +0200

    Removal of efzrevival_scanner as a whole - that thing sucked.

--- include/core/version.h
+++ include/core/version.h
@@ -1,7 +1,7 @@
 #pragma once
 
 // EFZ Training Mode version information
-#define EFZ_TRAINING_MODE_VERSION "0.9.4"
+#define EFZ_TRAINING_MODE_VERSION "0.9.5"
 
 // Build timestamp (from compiler)
 #define EFZ_TRAINING_MODE_BUILD_DATE __DATE__
